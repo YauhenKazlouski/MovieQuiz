149ff1de5461f22f04df1900f7ec2ff199f10b23
YauhenKazlouski <yauhenkazlouskiswift@gmail.com>
2025-01-02 17:39:16 +0300

    Added MoviesLoader

--- MovieQuiz/Models/MostPopularMovies.swift
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
-struct MostPopularMovies {
+struct MostPopularMovies: Codable {
     let errorMessage: String
     let items: [MostPopularMovie]
 }
 
-struct MostPopularMovie {
+struct MostPopularMovie: Codable {
     let title: String
     let rating: String
     let imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case rating = "imDbRating"
+        case imageURL = "image"
+    }
 }
